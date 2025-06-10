dc5a7666884933b29794edf16934ff0d1e934268
Олег Степанов <stepanov.2401@mail.ru>
2025-06-10 21:12:33 +0300

    add src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+def get_mask_card_number(card_numder: str) -> str:
+    """Функция маскирует цифры карты и разбивает на 4 блока"""
+    masked_card = card_numder[:4] + " " + card_numder[4:6] + "**" + " **** " + card_numder[-4:]
+    return masked_card
+
+
+print(get_mask_card_number("1738192038476129"))
+
+
+def get_mask_account(account_num: str):
+    """Функция маскирует цифры счета, кроме последних четырех"""
+    mask = "**"
+    result = str(mask + account_num[-4:])
+    return result
+
+
+print(get_mask_account("192419419461"))
