5f779466f38da250a6892fc4ca358059e6c4d501
Олег Степанов <stepanov.2401@mail.ru>
2025-07-03 08:17:24 +0300

    creat docstring in func ilter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,6 @@
 def filter_by_state(list_of_dict: list, state="EXECUTED") -> list:
+    """Функция фильтрует значение словарей по ключу"""
+
     filtered_list = []
     for dict_item in list_of_dict:
         if dict_item.get("state") == state:
