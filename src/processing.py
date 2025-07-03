639aed2882894bd9cd37fe1f53df47e961414437
Олег Степанов <stepanov.2401@mail.ru>
2025-07-03 07:17:50 +0300

    reformated file processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,11 @@
-def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
+def filter_by_state(list_of_dict: list, state="EXECUTED") -> list:
     filtered_list = []
     for dict_item in list_of_dict:
-         if dict_item.get('state') == state:
+        if dict_item.get("state") == state:
             filtered_list.append(dict_item)
-         else:
-             continue
+        else:
+            continue
     return filtered_list
-
 
 
 def sort_by_date(list_of_date: list[dict], rev: bool = True) -> list[dict]:
@@ -14,4 +13,3 @@ def sort_by_date(list_of_date: list[dict], rev: bool = True) -> list[dict]:
 
     date_list = sorted(list_of_date, key=lambda i: i["date"], reverse=rev)
     return date_list
-
