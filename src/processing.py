635d8ef72ca4e8fa418bb81897c99c9cb70038c9
Олег Степанов <stepanov.2401@mail.ru>
2025-07-03 07:11:07 +0300

    changed func filter_by_state in processing

--- src/processing.py
+++ src/processing.py
@@ -1,23 +1,17 @@
-def filter_by_state(list_of_status_state: list[dict],  state: str = "EXECUTED") -> list[dict]:
-    """Функция фильтрует статус операции"""
+def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
+    filtered_list = []
+    for dict_item in list_of_dict:
+         if dict_item.get('state') == state:
+            filtered_list.append(dict_item)
+         else:
+             continue
+    return filtered_list
 
-    operation_executed = []
-    operation_canceled = []
-
-    for operation in list_of_status_state:
-        if operation["state"] == "EXECUTED":
-            operation_executed.append(operation)
-        elif operation["state"] == "CANCELED":
-            operation_canceled.append(operation)
-    if state == "EXECUTED":
-        return operation_executed
-    elif state == "CANCELED":
-        return operation_canceled
 
 
 def sort_by_date(list_of_date: list[dict], rev: bool = True) -> list[dict]:
     """Функция сортировки элементов списка по убыванию даты"""
 
-    sorted_list_of_date = sorted(list_of_date, key=lambda i: i["date"], reverse=rev)
-    return sorted_list_of_date
+    date_list = sorted(list_of_date, key=lambda i: i["date"], reverse=rev)
+    return date_list
 
