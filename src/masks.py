e8e4cafd4651fad0ec9d00008c6f2d7195d190bc
Олег Степанов <stepanov.2401@mail.ru>
2025-07-09 22:40:43 +0300

    creat new test mask account

--- src/masks.py
+++ src/masks.py
@@ -9,8 +9,12 @@ print(get_mask_card_number("1596837868705199"))
 
 def get_mask_account(account_num: str):
     """Функция маскирует цифры счета, кроме последних четырех"""
+    if len(account_num) != 20:
+        return "Недопустимая длина счета"
+    if not account_num.isdigit():
+        return "Счет содержит недопустимые символы"
     mask = "**"
     result = str(mask + account_num[-4:])
     return result
 
-print(get_mask_account("64686473678894779589"))
+
