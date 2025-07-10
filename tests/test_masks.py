dd9041554865137b81f18fbdca85cc2397ed7b3b
Олег Степанов <stepanov.2401@mail.ru>
2025-07-10 19:30:43 +0300

    change module test_processing

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -23,6 +23,7 @@ def test_get_mask_account(value, expected):
     ('15968378 68705199', 'Счет содержит недопустимые символы'),
     ('1596837868705yrt', 'Счет содержит недопустимые символы'),
     ('159hg37868705yrt', 'Счет содержит недопустимые символы'),
+    ('1596837868706587', '1596 83** **** 6587'),
 ])
 def test_get_mask_card_number(value, expected):
     assert get_mask_card_number(value) == expected
