e1b12e6e4b743c0633fad9b605871d888bf6a8cf
Олег Степанов <stepanov.2401@mail.ru>
2025-07-30 09:27:43 +0300

    change module test_decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,49 +1,22 @@
 import pytest
-
 from src.decorators import log
 
-
-def test_log():
-    @log(filename="my_log.txt")
-    def summa(x, y):
-        return x + y
-
-    result_summa = summa(1, 2)
-    assert f"Функция my_function ок. Результат: {result_summa}" == "Функция my_function ок. Результат: 3"
-
-    def sb(x, y):
-        return x - y
-
-    result_sb = sb(2, 1)
-    assert f"Функция my_function ок. Результат: {result_sb}" == "Функция my_function ок. Результат: 1"
-
-    def division_corr(x, y):
-        return x / y
-
-    result_division_corr = division_corr(4, 2)
-    assert f"Функция my_function ок. Результат: {result_division_corr}" == "Функция my_function ок. Результат: 2.0"
-
-    def div_zero(x, y):
-        return x / y
-
-    with pytest.raises(ZeroDivisionError):
-        div_zero(2, 0)
-
-
-def test_decorator_cupsys(capsys):
-    """Тестирование декоратора с выходом ошибки с фикстурой cupsys"""
-    with pytest.raises(Exception):
-        my_func = my_function()
-        captured = my_func.readouterr()
-        assert captured.out == Exception
-
-
 @log()
-def faulty_function(x, y):
+def my_function(x, y):
     return x / y
 
+# Тест на успешное выполнение функции
 
-def test_faulty_function_logs_error(capsys):
-    faulty_function(1, 0)
+def test_my_function_success(capsys):
+    result = my_function(4, 2)
+    assert result == 2
     captured = capsys.readouterr()
-    assert "faulty_function error: division by zero" in captured.out
+    assert "my_function ok. Результат: 2" in captured.out
+
+# Тест на обработку ошибки деления на ноль
+
+def test_my_function_division_by_zero(capsys):
+    with pytest.raises(ZeroDivisionError):
+        my_function(4, 0)
+    captured = capsys.readouterr()
+    assert "my_function error: division by zero. Inputs: (4, 0), {}" in captured.out
